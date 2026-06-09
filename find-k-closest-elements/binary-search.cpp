52eee6fedc749a46928c60804d9694e5fcf9b06d
Abhishek Kumar <abhishek1kumar@icloud.com>
2026-06-09 08:55:08 +0530

    added document and reference binary search solution

---
+++ find-k-closest-elements/binary-search.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  vector<int> findClosestElements(vector<int> &arr, int k, int x) {
+    int low = 0, high = arr.size() - k;
+    while (low < high) {
+      int mid = (low + high) / 2;
+      if (x - arr[mid] > arr[mid + k] - x) {
+        low = mid + 1;
+      } else {
+        high = mid;
+      }
+    }
+    vector<int> result = vector<int>(arr.begin() + low, arr.begin() + low + k);
+    return result;
+  }
+};
